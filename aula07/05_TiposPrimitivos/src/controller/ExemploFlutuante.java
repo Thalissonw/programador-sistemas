1e9041465030070e81bafb68b5bf2db63efc1fb4
Thalissonw <44246736+Thalissonw@users.noreply.github.com>
2018-10-18 21:15:48 -0300

    Add files via upload

---
+++ aula07/05_TiposPrimitivos/src/controller/ExemploFlutuante.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class ExemploFlutuante {
+ public static void maind(String[] angs) {
+	 double salario, aumento , novoSalario;
+	 
+	       salario = 2000.00;
+	       aumento = 0.15;
+	       novoSalario = salario + (aumento * salario);
+	       System.out.println("NOVO Salário R$" + novoSalario);
+	 
+ }
+}
