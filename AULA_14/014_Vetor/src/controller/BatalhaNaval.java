de9dd1ffc038b6d14b124a451f8983e3cfdb4653
Thalissonw <44246736+Thalissonw@users.noreply.github.com>
2018-10-29 21:38:27 -0300

    Add files via upload

---
+++ AULA_14/014_Vetor/src/controller/BatalhaNaval.java
@@ -0,0 +1,31 @@
+package controller;
+
+import util.Teclado;
+
+public class BatalhaNaval {
+
+	public static void main(String[] args) {
+
+		// primeiro indece da linha, depois o indece da coluna
+		String arena[][] = new String[4][5];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 5; j++) {
+				arena[i][j] = "Água";
+			}
+		}
+		arena[2][2] = "Barco";
+		arena[1][3] = "Submarino";
+		arena[3][3] = "Porta-Aviões";
+		arena[0][2] = "Navio";
+
+		int linha, coluna;
+		
+		
+		linha = Teclado.lerInt("Digite a coordenada DA LINHA:");
+		coluna = Teclado.lerInt("Digite a coordenada da coluna:");
+
+		System.out.println("Voê acertou:" + arena[linha][coluna]);
+
+	}
+}
