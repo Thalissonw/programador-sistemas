99f0f51768e040948e06793108428a4d6a4432d9
Thalissonw <44246736+Thalissonw@users.noreply.github.com>
2018-11-01 20:49:04 -0300

    Add files via upload

---
+++ AULA_17/017_VerdadeiroFalso/src/Controller/Teste.java
@@ -0,0 +1,24 @@
+package Controller;
+
+public class Teste {
+  public static void main(String[] args) {
+	  boolean b = false;
+	  boolean a = true;
+	  boolean c = b && !b;
+	  if (c) {
+		  System.out.println(c);
+		  
+	  }
+	  else
+	  {
+		  System.out.println(c);
+		  
+	  }
+  
+  }
+     /*if (a != b ) {
+      * system.out.println("Diferente")
+      */
+	}
+
+
